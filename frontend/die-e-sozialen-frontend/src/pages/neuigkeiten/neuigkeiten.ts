d52176b0dccfe8c1fbd900cb9dba7c6c09fb341c
= <3kirchha@informatik.uni-hamburg.de>
2019-04-06 19:25:46 +0200

    Email layout fix

--- frontend/die-e-sozialen-frontend/src/pages/neuigkeiten/neuigkeiten.ts
+++ frontend/die-e-sozialen-frontend/src/pages/neuigkeiten/neuigkeiten.ts
@@ -1,84 +1,24 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController, Platform, ViewController, Loading, Modal, Form } from 'ionic-angular';
-import { ResourcesProvider } from '../../providers/resources/resources';
-import { HttpClient } from '@angular/common/http';
-import { FileEncryption } from '@ionic-native/file-encryption/ngx';
-import { PARAMETERS } from '@angular/core/src/util/decorators';
-import { AuthProvider } from '../../providers/auth/auth';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-// /**
-//  * Generated class for the NeuigkeitenPage page.
-//  *
-//  * See https://ionicframework.com/docs/components/#navigation for more info on
-//  * Ionic pages and navigation.
-//  */
+/**
+ * Generated class for the neuigkeitenPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
 
 @IonicPage()
 @Component({
   selector: 'page-neuigkeiten',
-  templateUrl: 'neuigkeiten.html'
+  templateUrl: 'neuigkeiten.html',
 })
 export class NeuigkeitenPage {
-  loggedIn: boolean = this.authProvider.loggedIn;
 
-  messages: JSON;
-  constructor(
-    //private fileEncryption: FileEncryption, 
-    public navCtrl: NavController, 
-    public navParams: NavParams, 
-    public httpClient: HttpClient,
-    public modalCtrl: ModalController,
-    public authProvider: AuthProvider) {
-
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    var resources = new ResourcesProvider(this.httpClient);
-    resources.getMessages().subscribe(response => {
-      console.log(response)
-      this.messages = response;
-    })
-  }
-
-  openModal(message) {
-
-    console.log(message);
-    let modal = this.modalCtrl.create(ModalContentPage, {message: message});
-    modal.present();
-  }
-
-  login(form: Form) {
-    console.log(form);
-    this.authProvider.login('testUser');
-  }
-
-  ngDoCheck() {
-    this.loggedIn = this.authProvider.loggedIn ? true : false;
-  }
-}
-
-@Component({
-  templateUrl: 'modal-content.html'
-})
-export class ModalContentPage {
-  message = {};
-  messages: JSON;
-  loading: boolean = false;
-
-  constructor(
-    public platform: Platform,
-    public navCtrl: NavController, 
-    public navParams: NavParams, 
-    public viewCtrl: ViewController,
-    public httpClient: HttpClient
-  ) {
-  }
-
-  ionViewDidLoad() {
-    this.message = this.navParams.get('message')
-  }
-
-  dismiss() {
-    this.viewCtrl.dismiss();
+    console.log('ionViewDidLoad NeuigkeitenPage');
   }
 }
