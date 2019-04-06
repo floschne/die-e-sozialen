127cdbbdad0ff46cc5f0d1238412506b6798e54d
floschne <florian.schneider.1992@gmx.de>
2019-04-06 15:30:36 +0200

    implemented getMessages endpoint

--- backend/src/main/java/org/dieesozialen/api/controller/MainController.java
+++ backend/src/main/java/org/dieesozialen/api/controller/MainController.java
@@ -3,13 +3,16 @@ package org.dieesozialen.api.controller;
 
 import io.swagger.annotations.Api;
 import org.dieesozialen.db.repos.QuoteRepo;
+import org.dieesozialen.entity.Message;
 import org.dieesozialen.entity.Quote;
+import org.dieesozialen.service.GMailService;
 import org.dieesozialen.service.QuoteApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.List;
 
 @RestController
@@ -20,11 +23,13 @@ public class MainController {
 
     private final QuoteApi quoteApi;
     private final QuoteRepo quoteRepo;
+    private final GMailService gMailService;
 
     @Autowired
-    public MainController(QuoteApi quoteApi, QuoteRepo quoteRepo) {
+    public MainController(QuoteApi quoteApi, QuoteRepo quoteRepo, GMailService gMailService) {
         this.quoteApi = quoteApi;
         this.quoteRepo = quoteRepo;
+        this.gMailService = gMailService;
     }
 
     /**
@@ -70,5 +75,10 @@ public class MainController {
         this.quoteRepo.deleteAll();
     }
 
+
+    @RequestMapping(value = "/getMessages", method = RequestMethod.GET)
+    public List<Message> getMessages() throws GeneralSecurityException, IOException {
+        return this.gMailService.getMessages();
+    }
 
 }
