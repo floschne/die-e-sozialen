b0753a88de2d8e98f5293ae4eb3008564eebdb54
Schoenherr <Levente.Schoenherr@otto.de>
2019-04-06 17:21:39 +0200

    Mulitple icons possible now

--- frontend/die-e-sozialen-frontend/src/pages/karte/karte.ts
+++ frontend/die-e-sozialen-frontend/src/pages/karte/karte.ts
@@ -17,6 +17,7 @@ import { Map, latLng, tileLayer, Layer, Marker, Icon, Circle, Polygon } from 'le
 export class KartePage {
 
   map: Map;
+  maxzoom: 18;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -32,29 +33,40 @@ export class KartePage {
     this.map = new Map('mapid').setView([51.505, -0.09], 13);
     this.map.locate({
       setView: true,
-      maxZoom: 15
+      maxZoom: this.maxzoom
     }).on('locationfound', (e) => {
       console.log("Hab Dich!");
       console.log(this.map.getCenter());
-      this.setMarker(this.map.getCenter())
+      this.setMarker('loc', this.map.getCenter(), true, 'Ihr Standort')
     })  
 
     tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       // tslint:disable-next-line
-    maxZoom: 15
+    maxZoom: this.maxzoom
     }).addTo(this.map);
   }
 
-  setMarker( coords) {
+  setMarker( type, coords, popup, popuptext) {
+
+    const iconDict: {[icon: string]: string} = {
+      loc: '../assets/imgs/marker.png',
+      med: '../assets/imgs/hospital.png',
+      bed: '../assets/imgs/shelter.png'
+    }
+
     var markerIcon = new Icon({
-      iconUrl: '../assets/imgs/marker.png',
-      iconSize: [40, 40], // size of the icon
-      iconAnchor: [20, 40],
-      popupAnchor: [0, -40]
+      iconUrl: iconDict[type],
+      iconSize: [45, 45],
+      iconAnchor: [22, 45],
+      popupAnchor: [0, -45]
     });
 
     var marker = new Marker(coords, {icon: markerIcon}).addTo(this.map);
-    marker.bindPopup('Your location').openPopup();
+    marker.bindPopup(popuptext);
+
+    if (popup) {
+      marker.openPopup();  
+    }
   }
 
 }
